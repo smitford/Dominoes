a70b984b5f97da9a8294b067d523518306736f87
Andrey Kalmykov <jump145@mail.ru>
2016-02-16 14:06:42 +0300

    Structure

---
+++ Dominoes.AI/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominoes.AI
+{
+    public class GameLogic
+    {
+        public delegate void AImovesHandler (Move move);
+        event AImovesHandler AImoves;
+
+        public void PlayerMoves(Move move)
+        {
+
+        }
+
+
+    }
+}
